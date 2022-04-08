b9358837fc235f15e54b7b669a395ffbc2a65adc
Salmo Mascarenhas <salmo.cruz@gmail.com>
2022-04-07 22:40:44 -0300

    fix margin and padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,11 @@ const Home = () => {
     <section className="w-full h-screen bg-dunes bg-cover bg-center relative">
       <Image src={bgHome} className='z-[-1] object-cover md:h-full md:w-48 ' alt='background home image' />
       <div className='absolute w-128 h-1/3 top-1/2 left-1/2 p-6 bg-black/90 rounded-lg text-center transform -translate-x-1/2 -translate-y-1/2 z-[1000]' >
-        <p className='text-xl	text-white text-center	'>
+        <p className='text-xl	text-white text-center mb-10'>
           Verifique seus projetos de construção através da Check PLUS, e ganhe mais assertividade nas submissões a prefeitura!
           Preencha os dados no link ao abaixo e aumente sua eficiência!
         </p>
-        <a href={'https://forms.gle/n21zyeabdfgpicem7'} target='_blank' className="mt-10 text-white bg-orange-500 hover:bg-orange-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0">
+        <a href={'https://forms.gle/n21zyeabdfgpicem7'} target='_blank' className="text-white bg-orange-500 hover:bg-orange-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0">
           Solicitar análise
         </a>
       </div>
